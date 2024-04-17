e59f0bd92b8febd86103973e10ff3edec0b1aef5
grtt5 <grtt5@10.237.98.224>
2024-04-17 14:32:32 +0200

    Removed unused feature: save_mode

--- main.m
+++ main.m
@@ -8,12 +8,7 @@ Parameters = struct() ;
 %% Simulation mode selection
 filter_length = 32 ;  % Default: 32
 
-% Choose to either store simulation results in an external file, or to
-% discard the results at the end of the program
-% /!\ Work in progress, save_mode should be kept to true
-save_mode = true ; % Boolean, default: true
-% If save_mode is true, state the .mat file in which the results will be
-% stored
+% .mat file in which the results will be stored
 data_file = 'white_noise_results.mat' ;
 
 % Save the figures of results as .pdf files
@@ -21,7 +16,6 @@ save_figures = true ; % Boolean, default: true
 
 % Chosse to plot the curve of error vs sample for every simulation or to
 % hide it
-% (Only available if save_mode is false)
 plot_all_error_curves = false ; % Boolean, default: false
 
 %% Noise type selection
@@ -48,18 +42,16 @@ for i = 1:length(noise_types)
     Parameters.(noise_types{i}).HTLMS.theta = linspace(0, 2, sweep_sim_number) ;
 end
 
-%% Parsing existing results (save_mode)
-if save_mode
-    % Parsing existing results to avoid running the same simulation twice
-    [Parameters, tests_added] = Parse_existing_results(data_file, Parameters) ;
-    % tests_added is true if some simulations remain after parsing the
-    % existing results, false otherwise
-end
-
 %% Impulse response of the unknown system
 rng(1) ;
 Sh = randn(filter_length, 1) ;
 Sh = Sh/(3*sum(Sh)) ;  % Arbitrary scaling
+
+%% Parsing existing results
+% Parsing existing results to avoid running the same simulation twice
+[Parameters, tests_added] = Parse_existing_results(data_file, Parameters) ;
+% tests_added is true if some simulations remain after parsing the
+% existing results, false otherwise
 
 %% Algorithm testing procedure
 if tests_added
